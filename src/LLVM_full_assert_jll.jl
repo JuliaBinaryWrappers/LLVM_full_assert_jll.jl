d6faf14d297c68a1cd7843fa65f1fabfb295c899
jlbuild <juliabuildbot@gmail.com>
2026-02-05 15:28:43 +0000

    LLVM_full_assert_jll build 15.0.7+11

--- src/LLVM_full_assert_jll.jl
+++ src/LLVM_full_assert_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("LLVM_full_assert")
-JLLWrappers.@generate_main_file("LLVM_full_assert", UUID("6ec703ca-3f29-566b-9bb1-b5c9e844abaf"))
+JLLWrappers.@generate_main_file("LLVM_full_assert", Base.UUID("6ec703ca-3f29-566b-9bb1-b5c9e844abaf"))
 end  # module LLVM_full_assert_jll
